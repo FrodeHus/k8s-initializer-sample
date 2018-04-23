7535e101a70d48b84e3e3ce2b71892c5e1b5052b
Frode Hus <frode.hus@outlook.com>
2018-04-23 09:27:14 +0200

    watcher code

--- main.go
+++ main.go
@@ -1,15 +1,72 @@
 package main
 
 import (
+	"log"
 	"os"
 	"path/filepath"
+	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	extv1beta1 "k8s.io/api/extensions/v1beta1"
+
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/kubernetes"
+
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+const (
+	annotation      = "initializer.sample.io/ingress"
+	initializerName = "ingress.initializer.sample.io"
+)
+
 func main() {
+	var config *rest.Config
+	stop := make(chan struct{})
+	var err error
 	kubeconfig := filepath.Join(
 		os.Getenv("HOME"), ".kube", "config",
 	)
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		config, err = rest.InClusterConfig()
+	}
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	defer close(stop)
+	start(clientset, stop)
+	<-stop
+}
+
+func start(clientset *kubernetes.Clientset, stopCh <-chan struct{}) {
+	log.Print("Starting ingress watcher...")
+	restClient := clientset.ExtensionsV1beta1().RESTClient()
+	watchList := cache.NewListWatchFromClient(restClient, "ingresses", corev1.NamespaceAll, fields.Everything())
+	resyncPeriod := 30 * time.Second
+	_, controller := cache.NewInformer(watchList, &extv1beta1.Ingress{}, resyncPeriod, cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			i := obj.(*extv1beta1.Ingress)
+			log.Printf("Ingress added: %s", i.GetName())
+		},
+		UpdateFunc: func(oldObj interface{}, newObj interface{}) {
+			existing := oldObj.(*extv1beta1.Ingress)
+			updated := newObj.(*extv1beta1.Ingress)
+			if existing.GetResourceVersion() == updated.GetResourceVersion() {
+				return
+			}
+			log.Printf("Ingress %s v%s updated: v%s", existing.GetName(), existing.GetResourceVersion(), updated.GetResourceVersion())
+		},
+		DeleteFunc: func(deletedObj interface{}) {
+			i := deletedObj.(*extv1beta1.Ingress)
+			log.Printf("Ingress deleted: %s", i.GetName())
+		},
+	})
+
+	go controller.Run(stopCh)
 }
