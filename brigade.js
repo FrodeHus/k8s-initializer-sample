03f2117af9601eac5348a37812ca2a32e0ede114
Frode Hus <frode.hus@outlook.com>
2018-05-07 21:58:17 +0200

    added brigade

---
+++ brigade.js
@@ -0,0 +1,21 @@
+const { events, Job } = require("brigadier");
+
+events.on("push", function(e, project) {
+  console.log("received push for commit " + e.commit)
+
+  // Create a new job
+  var node = new Job("build-go")
+
+  // We want our job to run the stock Docker Python 3 image
+  node.image = "mcuadros/golang-arm:1.9-alpine"
+
+  // Now we want it to run these commands in order:
+  node.tasks = [
+    "cd /src",
+    "dep ensure",
+    "go build"
+  ]
+
+  // We're done configuring, so we run the job
+  node.run()
+})
